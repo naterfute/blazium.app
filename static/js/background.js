1e21548c06d5ede52224e0d57b513bd944b3bc9e
Randolph W. Aarseth II <bioblazepayne@gmail.com>
2024-10-11 05:27:00 -0700

    moved javascript from html to background.js

--- static/js/background.js
+++ static/js/background.js
@@ -69,6 +69,51 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   },"retina_detect":true
 });
+
+function fetchReleaseInfo() {
+  const protocol = window.location.protocol;
+  const host = window.location.host;
+  const apiUrl = `${protocol}//${host}/api/mirrorlist/latest/json`;
+
+  console.log("Fetching from URL:", apiUrl);  // Add this line to check the URL
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const releaseMessage = document.getElementById('release-message');
+      
+      if (data.mirrors.length === 0) {
+        releaseMessage.innerHTML = 'Release is pending, visit our <a href="https://discord.gg/sZaf9KYzDp" style="color: #E83951;">Discord</a> for more information.';
+      } else {
+        const mirrors = data.mirrors.map(mirror => `
+          <li>
+            <a href="${mirror.download_url}" style="color: #E83951;">Download</a> |
+            SHA256: ${mirror.sha} |
+            Release Date: ${mirror.release_date}
+          </li>
+        `).join('');
+        
+        releaseMessage.innerHTML = `
+          <ul style="list-style: none; padding-left: 0; text-align: center;">
+            ${mirrors}
+          </ul>
+        `;
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching release information:', error);
+      document.getElementById('release-message').innerHTML = 'Error fetching release information. Please try again later.';
+    });
+}
+
+// Call the function to fetch the release info when the page loads
+window.onload = fetchReleaseInfo;
+
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
